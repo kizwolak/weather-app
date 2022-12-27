36abf6b892d4bf6f6a2643e16f71455b7b0cce27
Krzysztof <ki.zwolak@gmail.com>
2022-12-27 21:40:53 +0100

    Add unit conversion

--- src/index.js
+++ src/index.js
@@ -6,13 +6,26 @@ fetchData('Buenos Aires');
 searchIconLogic();
 
 const c = document.querySelector('.c');
-const f = document.querySelector('.p');
+const f = document.querySelector('.f');
 
 c.addEventListener('click', () => {
     const tempH1 = document.querySelector('.tempH1');
-    console.log(tempH1)
-    if (!tempH1.textContent.includes('C')) {
-        const celsiusTemp = tempH1.textContent.split('°');
-        console.log(celsiusTemp[0]);
+    console.log(tempH1.textContent)
+    if (!(tempH1.textContent.includes('C'))) {
+        const farenheitTemp = tempH1.textContent.split('°');
+        console.log(farenheitTemp[0]);
+        const result = (farenheitTemp[0] -32) * (5/9);
+        tempH1.textContent = `${result  }°C` 
     }
 });
+
+f.addEventListener('click', () => {
+    const tempH1 = document.querySelector('.tempH1');
+    if (!(tempH1.textContent.includes('F'))) {
+        const celsiusTemp = tempH1.textContent.split('°');
+        console.log(celsiusTemp[0]);
+        const result = (celsiusTemp[0] * (9/5)) + 32;
+        console.log(result);
+        tempH1.textContent = `${result  }°F` 
+    }
+})
